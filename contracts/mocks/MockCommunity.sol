37c237a87a1c3552ab2acba5df71d63cb85fd74b
Artem Subbotin <subbotin.artem@gmail.com>
2022-12-02 15:08:13 +0200

    step1

--- contracts/mocks/MockCommunity.sol
+++ contracts/mocks/MockCommunity.sol
@@ -41,7 +41,7 @@ contract MockCommunity is CommunityAccessMap, ICommunity {
         
     }
     
-    function getRoles(address member)public override view returns(uint8[] memory list){
+    function getRoles(address[] calldata members)public override view returns(uint8[][] memory list){
         // string[] memory list = new string[](5);
         // list[0] = 'owners';
         // list[1] = 'admins';
@@ -49,14 +49,18 @@ contract MockCommunity is CommunityAccessMap, ICommunity {
         // list[3] = 'sub-admins';
         // list[4] = 'unkwnowns';
         
-        list = roles[member];
-        
+        list = new uint8[][](members.length);
+
+        for(uint256 i = 0; i < members.length; i++) {
+            list[i] = roles[members[i]];
+        }
+
         return list;
         
     }
 
-    function getAddresses(uint8/* rolesIndex*/) public override pure returns(address[] memory){
-        address[] memory list = new address[](0);
+    function getAddresses(uint8[] memory/* rolesIndex*/) public override pure returns(address[][] memory){
+        address[][]memory list = new address[][](0);
         return list;
     }
 
--- hardhat.config.js
+++ hardhat.config.js
@@ -105,7 +105,7 @@ module.exports = {
           settings: {
             optimizer: {
               enabled: true,
-              runs: 10,
+              runs: 1,
             },
             metadata: {
               // do not include the metadata hash, since this is machine dependent
