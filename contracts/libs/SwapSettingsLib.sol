8002195b222554cab88f044059f65737f2866588
Artem Subbotin <subbotin.artem@gmail.com>
2023-03-07 14:21:35 +0200

    mumbai support

--- contracts/libs/SwapSettingsLib.sol
+++ contracts/libs/SwapSettingsLib.sol
@@ -19,8 +19,8 @@ library SwapSettingsLib {
                 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D, //uniswapRouter
                 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f  //uniswapRouterFactory
             );
-        } else if(chainId == 0x89) {
-            // Matic-QuickSwap
+        } else if(chainId == 0x89 || chainId == 0x13881) {
+            // Matic-QuickSwap and mumbai
             return( 
                 0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff, //uniswapRouter
                 0x5757371414417b8C6CAad45bAeF941aBc7d3Ab32  //uniswapRouterFactory
